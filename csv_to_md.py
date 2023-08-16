4335327c7d98f1c4bb7664620ce92658fe2d023a
danhumassmed <daniel.higgins@umassmed.edu>
2023-08-16 16:43:07 -0400

    changed from json to csv

--- csv_to_md.py
+++ csv_to_md.py
@@ -1,10 +1,14 @@
-import json
+import csv
 
 base_url = "https://dphiggs01.github.io/Wormcat_data"
 
-# Read JSON data from a file
-with open('annotation_list.json', 'r') as json_file:
-    data = json.load(json_file)
+# Read CSV data from a file
+csv_file = 'annotation_list.csv'
+data = []
+with open(csv_file, 'r') as csvfile:
+    csv_reader = csv.DictReader(csvfile)
+    for row in csv_reader:
+        data.append(row)
 
 # Sort the data by the "Order" field
 sorted_data = sorted(data, key=lambda x: int(x['order']))
@@ -17,8 +21,8 @@ for entry in sorted_data:
     file_link = f"[{entry['file_name']}]({base_url}/{entry['location_suffix']}/{entry['file_name']})"
     markdown += f"| {entry['short_desc']} | {file_link} |\n"
 
-# Write the Markdown table to a file or print it
+# Write the Markdown table to a file
 with open('annotation_list.md', 'w') as md_file:
     md_file.write(markdown)
 
-print("Markdown table generated and saved to 'output.md'")
+print("Markdown table generated and saved to 'annotation_list.md'")
